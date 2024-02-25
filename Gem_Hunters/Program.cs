7c84f29d3fd11fe8acc60f8792a9b6bddecdb8e3
Vansh1009 <vansh.palkhiwala10@gmail.com>
2024-02-25 18:20:09 -0500

    Making Player class to add all the activities of player such as direction to move, gem count

--- Gem_Hunters/Program.cs
+++ Gem_Hunters/Program.cs
@@ -12,6 +12,42 @@ public class Position
     }
 }
 
+public class Player
+{
+    public string Name { get; }
+    public Position Position { get; set; }
+    public int GemCount { get; set; }
+
+    public Player(string name, Position position)
+    {
+        Name = name;
+        Position = position;
+        GemCount = 0;
+    }
+
+    public void Move(char direction)
+    {
+        switch (direction)
+        {
+            case 'U':
+                Position.Y--;
+                break;
+            case 'D':
+                Position.Y++;
+                break;
+            case 'L':
+                Position.X--;
+                break;
+            case 'R':
+                Position.X++;
+                break;
+            default:
+                Console.WriteLine("Invalid direction input.");
+                break;
+        }
+    }
+}
+
 
 class Program
 {
