06e492a653538b66530c4bd35aede265a9efb27b
Vansh1009 <vansh.palkhiwala10@gmail.com>
2024-02-25 18:11:56 -0500

    MAking postion class to assign position

--- Gem_Hunters/Program.cs
+++ Gem_Hunters/Program.cs
@@ -1,5 +1,18 @@
 ﻿using System;
 
+public class Position
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Position(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
+
 class Program
 {
     static void Main(string[] args)
